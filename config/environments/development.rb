a070618ec7e020dd57e1d4b5f86f08d6228200f2
Amin_Isazadeh <amin.isazadeh@tamu.edu>
2023-02-23 00:31:26 +0000

    commit Procfile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "861075b555304e99b2044dedf36ef599.vfs.cloud9.us-east-2.amazonaws.com"
+
 end
+
+#"config/environments/development.rb" 71L, 2739C written
